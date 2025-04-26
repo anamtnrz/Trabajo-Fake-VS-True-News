d4f74bbf9a3027eb2c7bcd7a4daebb08672582a9
anamtnrz <114219938+anamtnrz@users.noreply.github.com>
2025-04-26 11:16:54 +0200

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,1 +1,15 @@
+#1- IMPORTAR LIBRERÍAS
+
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.metrics import accuracy_score, classification_report
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+
+
 
